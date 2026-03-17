fd008adb362ca6adac5d1913ac54d1045dcef70e
Aron <stadleraron@gmail.com>
2026-03-17 13:42:07 +0100

    Adde ProductCard component to home page
    images still need little tweakin

--- Frontend/vizsgaremek/src/pages/home/Home.tsx
+++ Frontend/vizsgaremek/src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import Slide from "../../components/Home/Slide/Slide";
 import DiscountSlide from "../../components/Home/DiscountSlide/DiscountSlide";
+import ProductCard from "../../components/Home/ProductCards/productcard";
 import "./Home.css";
 
 function Home() {
   return (
     <>
       <Slide />
+      <ProductCard />
       <DiscountSlide />
     </>
   );
