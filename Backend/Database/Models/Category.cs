f1190aa517ae5df3472114b2a7a3749841a0623d
Fox_Hunter <oliver.seres2006@gmail.com>
2026-03-17 10:34:16 +0100

    Added Put and Delete to Categories and Specs
    also added some authorization to delete and update products.. and changed some null refs

--- Backend/Database/Models/Category.cs
+++ Backend/Database/Models/Category.cs
@@ -9,7 +9,7 @@ namespace Database.Models
     public class Category
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public required string Name { get; set; }
 
         public ICollection<Product> Products { get; set; } = [];
     }
