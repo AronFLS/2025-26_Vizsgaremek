2dc4950b0816109d942a29aa9583201f01477727
Matusek6 <matecsondes@gmail.com>
2026-03-31 12:45:14 +0200

    Built in categories
    Added phones, macbooks and accessories category seed data to database
    --make sure to delete existing database and update it with newest migration for it to work--
    There will be more database changes in the close future, so this isn't the last migration

---
+++ Backend/Database/BuiltInCategories.cs
@@ -0,0 +1,9 @@
+namespace Database
+{
+  public class BuiltInCategories
+  {
+    public const string Phones = "phone";
+    public const string Macbooks = "macbook";
+    public const string Accessories = "accessories";
+  }
+}
